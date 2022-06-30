93311d33fa701025b722b0a9055c1c247c4bcfc8
NaYfront <nayfront@MacBook-Air-NaYfront.local>
2022-06-30 21:11:16 +0300

    initial commit

---
+++ SimbirsoftTask/MainModule/View/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  SimbirsoftTask
+//
+//  Created by NaYfront on 30.06.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
